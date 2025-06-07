c1b262a91399fa21277e8e19598ae3041c90901d
Felipe M <philbone@focused.cl>
2025-06-07 19:08:43 -0400

    Agrega ejemplo de traducción para las vistas Auth

---
+++ resources/lang/es/auth.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    "label_email" => "Correo Electrónico",
+    "label_password" => "Contraseña",
+    "remember_me" => "Recuerdame",
+    "forgot_password" => "¿Olvidaste tu contraseña?",
+    "forgot_password_text" => "¿Olvidaste tu contraseña? No hay problema. Indícanos tu correo electrónico y te enviaremos un enlace para restablecer la contraseña y podrás elegir una nueva.",
+    "email_password_reset_link" => "Enviar Enlace",
+    "register" => "Registrar",
+    "login" => "Ingresar",
+    "label_register_name" => "Nombre",
+    "label_confirm_password" => "Confirmar Contraseña",
+    "label_already_registered" => "¿Ya estás registrado?"
+];
