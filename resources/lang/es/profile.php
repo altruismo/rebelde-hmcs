2fba2349243c33db589ce0fb8164174478695193
Felipe M <philbone@focused.cl>
2025-06-07 15:12:41 -0400

    Agrega ejemplo de traducción para las vistas parciales

---
+++ resources/lang/es/profile.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'profile' => 'Perfil de Cliente',
+    'profile_information_title' => 'Información del perfil',
+    'profile_information_description' => "Actualiza tu nombre y dirección de correo.",
+    'name' => "Nombre",
+    'email' => "Correo electrónico",
+    'email_unverified' => "Tu dirección de correo no ha sido verificada.",
+    'email_resend' => "Click aquí para re-enviar el correo de verificación.",
+    'email_verification_send' => "Un nuevo enlace de verificación fue enviado a tu dirección de correo.",
+    'save' => "Guardar",
+    'saved' => "Guardado.",
+    'cancel' => "Cancelar",
+    'update_password' => 'Cambiar la Contraseña',
+    'update_password_description' => 'Asegúrate de usar una contraseña larga y aleatoria para mayor seguridad.',
+    'current_password' => 'Contraseña Actual',
+    'new_password' => 'Nueva Contraseña',
+    'confirm_password' => 'Confirma la Nueva Contraseña',
+    'placeholder_password' => 'Contraseña',
+    'delete_account' => 'Eliminar Cuenta',
+    'delete_warn' => 'Una vez eliminada su cuenta, todos sus recursos y datos se eliminarán permanentemente. Antes de eliminar su cuenta, descargue cualquier dato o información que desee conservar.',
+    'delete_final_ask' => '¿Estás seguro que quieres borrar tu cuenta para siempre?',
+    'delete_final_warn' => 'Una vez tu cuenta es eliminada, todos los recursos y datos son permanentemente removidos. Por favor ingresa tu contraseña para confirmar que deseas eliminar tu cuenta de forma completa y permanente.'
+];
