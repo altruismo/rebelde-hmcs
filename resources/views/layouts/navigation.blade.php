18ed8f91f8068a7a576095657aaa6ae267bd0f6e
Felipe M <philbone@focused.cl>
2025-06-26 00:51:05 -0400

    Mejora el Dropdown Menú del Usuario Cliente

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -2,6 +2,7 @@
 
     <div class="flex flex-wrap justify-between items-center">
         <div class="flex justify-start items-center">
+            <!-- Toggle Aside Navigation -->
             <button
                 data-drawer-target="drawer-navigation"
                 data-drawer-toggle="drawer-navigation"
@@ -41,7 +42,8 @@
 
         </div>
         <div class="flex items-center lg:order-2">
-            <button
+            <!-- Toggle Search -->
+            <!-- <button
                 type="button"
                 data-drawer-toggle="drawer-navigation"
                 aria-controls="drawer-navigation"
@@ -50,8 +52,9 @@
                 <svg aria-hidden="true" class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                     <path clip-rule="evenodd" fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"></path>
                 </svg>
-            </button>
+            </button> -->
 
+            <!-- Toggle Lang -->
              <div title="Cambiar el lenguaje" class="mr-4 sm:flex sm:items-center sm:ms-6">
                 <!-- <span class="text-sm dark:text-heaven">{{ __('navigation.lang') }}:</span> -->
                 <a href="{{ route('lang.switch', 'es') }}" class="dark:text-dark-heaven hover:underline hover:text-dim">ES</a> |
@@ -85,7 +88,7 @@
                         d="M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6zM10 18a3 3 0 01-3-3h6a3 3 0 01-3 3z"></path>
                 </svg>
             </button>
-            <!-- Dropdown menu -->
+            <!-- Notifications Dropdown Menu -->
             <div
                 class="hidden overflow-hidden z-50 my-4 max-w-sm text-base list-none bg-white rounded divide-y divide-gray-100 shadow-lg dark:divide-gray-600 dark:bg-gray-700 rounded-xl"
                 id="notification-dropdown">
@@ -310,7 +313,7 @@
                         d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path>
                 </svg>
             </button>
-            <!-- Dropdown menu -->
+            <!-- Apps Dropdown menu -->
             <div
                 class="hidden overflow-hidden z-50 my-4 max-w-sm text-base list-none bg-white rounded divide-y divide-gray-100 shadow-lg dark:bg-gray-700 dark:divide-gray-600 rounded-xl"
                 id="apps-dropdown">
@@ -492,103 +495,7 @@
                     src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/michael-gough.png"
                     alt="user photo" />
             </button>
-            <!-- Dropdown menu -->
-            <div
-                class="hidden z-50 my-4 w-56 text-base list-none bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600 rounded-xl"
-                id="dropdown">
-                <div class="py-3 px-4">
-                    <span
-                        class="block text-sm font-semibold text-gray-900 dark:text-white">Neil Sims</span>
-                    <span
-                        class="block text-sm text-gray-900 truncate dark:text-white">name@flowbite.com</span>
-                </div>
-                <ul
-                    class="py-1 text-gray-700 dark:text-gray-300"
-                    aria-labelledby="dropdown">
-                    <li>
-                        <a
-                            href="#"
-                            class="block py-2 px-4 text-sm hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white">My profile</a>
-                    </li>
-                    <li>
-                        <a
-                            href="#"
-                            class="block py-2 px-4 text-sm hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white">Account settings</a>
-                    </li>
-                </ul>
-                <ul
-                    class="py-1 text-gray-700 dark:text-gray-300"
-                    aria-labelledby="dropdown">
-                    <li>
-                        <a
-                            href="#"
-                            class="flex items-center py-2 px-4 text-sm hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"><svg
-                                class="mr-2 w-5 h-5 text-gray-400"
-                                fill="currentColor"
-                                viewBox="0 0 20 20"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <path
-                                    fill-rule="evenodd"
-                                    d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
-                                    clip-rule="evenodd"></path>
-                            </svg>
-                            My likes</a>
-                    </li>
-                    <li>
-                        <a
-                            href="#"
-                            class="flex items-center py-2 px-4 text-sm hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"><svg
-                                class="mr-2 w-5 h-5 text-gray-400"
-                                fill="currentColor"
-                                viewBox="0 0 20 20"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <path
-                                    d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z"></path>
-                            </svg>
-                            Collections</a>
-                    </li>
-                    <li>
-                        <a
-                            href="#"
-                            class="flex justify-between items-center py-2 px-4 text-sm hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
-                            <span class="flex items-center">
-                                <svg
-                                    aria-hidden="true"
-                                    class="mr-2 w-5 h-5 text-primary-600 dark:text-primary-500"
-                                    fill="currentColor"
-                                    viewBox="0 0 20 20"
-                                    xmlns="http://www.w3.org/2000/svg">
-                                    <path
-                                        fill-rule="evenodd"
-                                        d="M12.395 2.553a1 1 0 00-1.45-.385c-.345.23-.614.558-.822.88-.214.33-.403.713-.57 1.116-.334.804-.614 1.768-.84 2.734a31.365 31.365 0 00-.613 3.58 2.64 2.64 0 01-.945-1.067c-.328-.68-.398-1.534-.398-2.654A1 1 0 005.05 6.05 6.981 6.981 0 003 11a7 7 0 1011.95-4.95c-.592-.591-.98-.985-1.348-1.467-.363-.476-.724-1.063-1.207-2.03zM12.12 15.12A3 3 0 017 13s.879.5 2.5.5c0-1 .5-4 1.25-4.5.5 1 .786 1.293 1.371 1.879A2.99 2.99 0 0113 13a2.99 2.99 0 01-.879 2.121z"
-                                        clip-rule="evenodd"></path>
-                                </svg>
-                                Pro version
-                            </span>
-                            <svg
-                                aria-hidden="true"
-                                class="w-5 h-5 text-gray-400"
-                                fill="currentColor"
-                                viewBox="0 0 20 20"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <path
-                                    fill-rule="evenodd"
-                                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                                    clip-rule="evenodd"></path>
-                            </svg>
-                        </a>
-                    </li>
-                </ul>
-                <ul
-                    class="py-1 text-gray-700 dark:text-gray-300"
-                    aria-labelledby="dropdown">
-                    <li>
-                        <a
-                            href="#"
-                            class="block py-2 px-4 text-sm hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign out</a>
-                    </li>
-                </ul>
-            </div>
+            <x-client-user.profile-dropdown />
         </div>
     </div>
 </nav>
