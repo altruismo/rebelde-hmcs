7c459be9346b602ffd9ebf024778527973a11bd2
Felipe M <philbone@focused.cl>
2025-07-11 00:58:24 -0400

    Separa los components quick-access, active-services, carousel y search-bar

---
+++ resources/views/components/client-user/active-services.blade.php
@@ -0,0 +1,68 @@
+<div class="rounded-lg shadow pb-1 h-96">
+    <div class="w-full h-full overflow-y-scroll bg-white rounded-lg dark:bg-dark-deep dark:border-gray-700 scrollbar">
+        <h5 class="flex justify-between p-2 px-4 rounded-t-lg text-base font-semibold text-gray-900 md:text-xl dark:text-white bg-clear dark:bg-dark-brown">
+            Servicios Activos
+            <a href="#" class="inline-flex items-center text-xs font-normal text-gray-500 hover:underline dark:text-gray-400">                
+                View All...
+            </a>
+        </h5>
+        <div class="p-4 pt-1 dark:bg-dark-deep">
+        <p class="text-sm font-normal text-gray-500 dark:text-gray-400">Servicios con suscripción activa o recientemente suspendidos.</p>
+        <ul class="my-4 space-y-3">
+            <li>
+                <a href="#" class="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-dark-brown dark:hover:bg-dark-dim dark:text-white dark:hover:text-secondary">
+                    <svg class="w-6 h-6 p-1 rounded-full text-heaven bg-gradient-to-tr to-dim from-green-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12a1 1 0 0 0-1 1v4a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1v-4a1 1 0 0 0-1-1M5 12h14M5 12a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v4a1 1 0 0 1-1 1m-2 3h.01M14 15h.01M17 9h.01M14 9h.01"/>
+                    </svg>
+                    <span class="flex-1 ms-3 whitespace-nowrap text-base overflow-auto">Web Hosting Básico - condorito.cl</span>
+                    <span class="inline-flex items-center justify-center px-2 py-0.5 sm:p-2 ms-3 text-xs font-medium text-gray-500 bg-gray-200 rounded-sm dark:bg-dark-heaven dark:text-secondary">active</span>
+                </a>
+            </li>
+            <li>
+                <a href="#" class="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-dark-brown dark:hover:bg-dark-dim dark:text-white dark:hover:text-secondary">
+                    <svg class="w-6 h-6 p-1 rounded-full text-heaven bg-gradient-to-tr to-dim from-green-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12a1 1 0 0 0-1 1v4a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1v-4a1 1 0 0 0-1-1M5 12h14M5 12a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v4a1 1 0 0 1-1 1m-2 3h.01M14 15h.01M17 9h.01M14 9h.01"/>
+                    </svg>
+                    <span class="flex-1 ms-3 whitespace-nowrap text-base overflow-auto">Web Hosting Inicia - sale.com </span>
+                    <span class="inline-flex items-center justify-center px-2 py-0.5 sm:p-2 ms-3 text-xs font-medium text-gray-500 bg-gray-200 rounded-sm dark:bg-dark-clear dark:text-secondary">suspended</span>
+                </a>
+            </li>
+            <li>
+                <a href="#" class="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-dark-brown dark:hover:bg-dark-dim dark:text-white dark:hover:text-secondary">                    
+                    <svg class="w-6 h-6 p-1 rounded-full text-heaven bg-gradient-to-tr to-orange-500 from-orange-900" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12a1 1 0 0 0-1 1v4a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1v-4a1 1 0 0 0-1-1M5 12h14M5 12a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v4a1 1 0 0 1-1 1m-2 3h.01M14 15h.01M17 9h.01M14 9h.01"/>
+                    </svg>
+                    <span class="flex-1 ms-3 whitespace-nowrap text-base overflow-auto">VPS Hosting(Full) - vps.focused.cl</span>
+                    <span class="inline-flex items-center justify-center px-2 py-0.5 sm:p-2 ms-3 text-xs font-medium text-gray-500 bg-gray-200 rounded-sm dark:bg-dark-heaven dark:text-secondary">active</span>
+                </a>
+            </li>
+            <li>
+                <a href="#" class="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-dark-brown dark:hover:bg-dark-dim dark:text-white dark:hover:text-secondary">
+                    <svg class="w-6 h-6 p-1 rounded-full text-heaven bg-gradient-to-tr to-blue-400 from-blue-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12a1 1 0 0 0-1 1v4a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1v-4a1 1 0 0 0-1-1M5 12h14M5 12a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v4a1 1 0 0 1-1 1m-2 3h.01M14 15h.01M17 9h.01M14 9h.01"/>
+                    </svg>
+                    <span class="flex-1 ms-3 whitespace-nowrap text-base overflow-auto">VM Droplet - vm.comercialx.cl</span>
+                    <span class="inline-flex items-center justify-center px-2 py-0.5 sm:p-2 ms-3 text-xs font-medium text-gray-500 bg-gray-200 rounded-sm dark:bg-dark-heaven dark:text-secondary">active</span>   
+                </a>
+            </li>
+            <li>
+                <a href="#" class="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-dark-brown dark:hover:bg-dark-dim dark:text-white dark:hover:text-secondary">
+                    <svg class="w-6 h-6 p-1 rounded-full text-heaven bg-gradient-to-tr to-purple-500 from-purple-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12a1 1 0 0 0-1 1v4a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1v-4a1 1 0 0 0-1-1M5 12h14M5 12a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v4a1 1 0 0 1-1 1m-2 3h.01M14 15h.01M17 9h.01M14 9h.01"/>
+                    </svg>
+                    <span class="flex-1 ms-3 whitespace-nowrap text-base overflow-auto">Sitio Web(DC) - www.loremipsum.cl</span>
+                    <span class="inline-flex items-center justify-center px-2 py-0.5 sm:p-2 ms-3 text-xs font-medium text-gray-500 bg-gray-200 rounded-sm dark:bg-dark-heaven dark:text-secondary">active</span>
+                </a>
+            </li>
+        </ul>
+        <div>
+            <a href="#" class="inline-flex items-center text-xs font-normal text-gray-500 hover:underline dark:text-gray-400">
+                <svg class="w-3 h-3 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7.529 7.988a2.502 2.502 0 0 1 5 .191A2.441 2.441 0 0 1 10 10.582V12m-.01 3.008H10M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                </svg>
+                View All Services...</a>
+        </div>
+    </div>
+    </div>
+
+</div>
---
+++ resources/views/components/client-user/quick-access.blade.php
@@ -0,0 +1,50 @@
+<div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mb-4 text-dim dark:text-dark-dim">
+    <!-- Servicios -->
+    <div class="p-8 pb-1 rounded-lg shadow grid grid-flow-col grid-rows-2 grid-cols-2 !bg-gradient-to-t !from-white !to-clear/50 dark:!from-dark-deep dark:!via-dark-brown/15 dark:!to-dark-deep">
+        <div class="text-center text-2xl lg:text-4xl font-bold z-30 pt-8">Services</div>
+        <div class="text-center text-8xl font-bold pt-8 text-dim dark:text-dark-dim">6</div>
+        <div class="text-right opacity-20">
+            <svg class="w-full h-full text-dim dark:text-dark-dim" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                <path fill-rule="evenodd" d="M5 5a2 2 0 0 0-2 2v3a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1V7a2 2 0 0 0-2-2H5Zm9 2a1 1 0 1 0 0 2h.01a1 1 0 1 0 0-2H14Zm3 0a1 1 0 1 0 0 2h.01a1 1 0 1 0 0-2H17ZM3 17v-3a1 1 0 0 1 1-1h16a1 1 0 0 1 1 1v3a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2Zm11-2a1 1 0 1 0 0 2h.01a1 1 0 1 0 0-2H14Zm3 0a1 1 0 1 0 0 2h.01a1 1 0 1 0 0-2H17Z" clip-rule="evenodd" />
+            </svg>
+        </div>
+        <div class="text-right pt-24"><a href="#" class="mr-7"> view... </a></div>
+    </div>
+
+    <!-- Dominios Registrados -->
+    <div class="p-8 pb-1 rounded-lg shadow bg-milk dark:bg-[#271c0a] grid grid-flow-col grid-rows-2 grid-cols-2 !bg-gradient-to-t !from-white !to-clear/50 dark:!from-dark-deep dark:!via-dark-brown/15 dark:!to-dark-deep">
+        <div class="text-center text-2xl lg:text-4xl font-bold z-30 pt-8">Domains</div>
+        <div class="text-center text-8xl font-bold pt-8 text-dim dark:text-dark-dim">4</div>
+        <div class="text-right opacity-20">
+            <svg class="w-full h-full text-dim dark:text-dark-dim" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                <path fill-rule="evenodd" d="M8.64 4.737A7.97 7.97 0 0 1 12 4a7.997 7.997 0 0 1 6.933 4.006h-.738c-.65 0-1.177.25-1.177.9 0 .33 0 2.04-2.026 2.008-1.972 0-1.972-1.732-1.972-2.008 0-1.429-.787-1.65-1.752-1.923-.374-.105-.774-.218-1.166-.411-1.004-.497-1.347-1.183-1.461-1.835ZM6 4a10.06 10.06 0 0 0-2.812 3.27A9.956 9.956 0 0 0 2 12c0 5.289 4.106 9.619 9.304 9.976l.054.004a10.12 10.12 0 0 0 1.155.007h.002a10.024 10.024 0 0 0 1.5-.19 9.925 9.925 0 0 0 2.259-.754 10.041 10.041 0 0 0 4.987-5.263A9.917 9.917 0 0 0 22 12a10.025 10.025 0 0 0-.315-2.5A10.001 10.001 0 0 0 12 2a9.964 9.964 0 0 0-6 2Zm13.372 11.113a2.575 2.575 0 0 0-.75-.112h-.217A3.405 3.405 0 0 0 15 18.405v1.014a8.027 8.027 0 0 0 4.372-4.307ZM12.114 20H12A8 8 0 0 1 5.1 7.95c.95.541 1.421 1.537 1.835 2.415.209.441.403.853.637 1.162.54.712 1.063 1.019 1.591 1.328.52.305 1.047.613 1.6 1.316 1.44 1.825 1.419 4.366 1.35 5.828Z" clip-rule="evenodd" />
+            </svg>
+        </div>
+        <div class="text-right pt-24"><a href="#" class="mr-7"> view... </a></div>
+    </div>
+
+    <!-- Tickets -->
+    <div class="p-8 pb-1 rounded-lg shadow bg-milk dark:bg-[#271c0a] grid grid-flow-col grid-rows-2 grid-cols-2 !bg-gradient-to-t !from-white !to-clear/50 dark:!from-dark-deep dark:!via-dark-brown/15 dark:!to-dark-deep">
+        <div class="text-center text-2xl lg:text-4xl font-bold z-30 pt-8">Tickets</div>
+        <div class="text-center text-8xl font-bold pt-8 text-dim dark:text-dark-dim">2</div>
+        <div class="text-right opacity-20">
+            <svg class="w-full h-full text-dim dark:text-dark-dim" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                <path fill-rule="evenodd" d="M12 2a7 7 0 0 0-7 7 3 3 0 0 0-3 3v2a3 3 0 0 0 3 3h1a1 1 0 0 0 1-1V9a5 5 0 1 1 10 0v7.083A2.919 2.919 0 0 1 14.083 19H14a2 2 0 0 0-2-2h-1a2 2 0 0 0-2 2v1a2 2 0 0 0 2 2h1a2 2 0 0 0 1.732-1h.351a4.917 4.917 0 0 0 4.83-4H19a3 3 0 0 0 3-3v-2a3 3 0 0 0-3-3 7 7 0 0 0-7-7Zm1.45 3.275a4 4 0 0 0-4.352.976 1 1 0 0 0 1.452 1.376 2.001 2.001 0 0 1 2.836-.067 1 1 0 1 0 1.386-1.442 4 4 0 0 0-1.321-.843Z" clip-rule="evenodd" />
+            </svg>
+        </div>
+        <div class="text-right pt-24"><a href="#" class="mr-7"> view... </a></div>
+    </div>
+
+    <!-- Invoices -->
+    <div class="p-8 pb-1 rounded-lg shadow bg-milk dark:bg-[#271c0a] grid grid-flow-col grid-rows-2 grid-cols-2 !bg-gradient-to-t !from-white !to-clear/50 dark:!from-dark-deep dark:!via-dark-brown/15 dark:!to-dark-deep">
+        <div class="text-center text-2xl lg:text-4xl font-bold z-30 pt-8">Invoices</div>
+        <div class="text-center text-8xl font-bold pt-8 text-dim dark:text-dark-dim">10</div>
+        <div class="text-right opacity-20">
+            <svg class="w-full h-full text-dim dark:text-dark-dim" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                <path fill="currentColor" d="M4 19v2c0 .5523.44772 1 1 1h14c.5523 0 1-.4477 1-1v-2H4Z" />
+                <path fill="currentColor" fill-rule="evenodd" d="M9 3c0-.55228.44772-1 1-1h8c.5523 0 1 .44772 1 1v3c0 .55228-.4477 1-1 1h-2v1h2c.5096 0 .9376.38314.9939.88957L19.8951 17H4.10498l.90116-8.11043C5.06241 8.38314 5.49047 8 6.00002 8H12V7h-2c-.55228 0-1-.44772-1-1V3Zm1.01 8H8.00002v2.01H10.01V11Zm.99 0h2.01v2.01H11V11Zm5.01 0H14v2.01h2.01V11Zm-8.00998 3H10.01v2.01H8.00002V14ZM13.01 14H11v2.01h2.01V14Zm.99 0h2.01v2.01H14V14ZM11 4h6v1h-6V4Z" clip-rule="evenodd" />
+            </svg>
+        </div>
+        <div class="text-right pt-24"><a href="#" class="mr-7"> view... </a></div>
+    </div>
+</div>
