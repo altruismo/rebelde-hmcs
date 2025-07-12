b6922f90b6d89fbbc958d016961a6bb81a7a4cc5
Felipe M <philbone@focused.cl>
2025-07-12 02:08:37 -0400

    Ubica los componentes Last Invoice y Recent Invoices en el home del escritorio cliente

--- resources/views/components/client-user/active-services.blade.php
+++ resources/views/components/client-user/active-services.blade.php
@@ -10,48 +10,48 @@
         <p class="text-sm font-normal text-gray-500 dark:text-gray-400">Servicios con suscripción activa o recientemente suspendidos.</p>
         <ul class="my-4 space-y-3">
             <li>
-                <a href="#" class="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-dark-brown dark:hover:bg-dark-dim dark:text-white dark:hover:text-secondary">
+                <a href="#" class="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-dark-brown/15 dark:hover:bg-dark-brown/50 dark:text-white">
                     <svg class="w-6 h-6 p-1 rounded-full text-heaven bg-gradient-to-tr to-dim from-green-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12a1 1 0 0 0-1 1v4a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1v-4a1 1 0 0 0-1-1M5 12h14M5 12a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v4a1 1 0 0 1-1 1m-2 3h.01M14 15h.01M17 9h.01M14 9h.01"/>
                     </svg>
                     <span class="flex-1 ms-3 whitespace-nowrap text-base overflow-auto">Web Hosting Básico - condorito.cl</span>
-                    <span class="inline-flex items-center justify-center px-2 py-0.5 sm:p-2 ms-3 text-xs font-medium text-gray-500 bg-gray-200 rounded-sm dark:bg-dark-heaven dark:text-secondary">active</span>
+                    <span class="w-24 inline-flex items-center justify-center px-2 py-0.5 sm:p-2 ms-3 text-xs font-medium text-haven bg-green-800 rounded-sm">active</span>
                 </a>
             </li>
             <li>
-                <a href="#" class="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-dark-brown dark:hover:bg-dark-dim dark:text-white dark:hover:text-secondary">
+                <a href="#" class="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-dark-brown/15 dark:hover:bg-dark-brown/50 dark:text-white">
                     <svg class="w-6 h-6 p-1 rounded-full text-heaven bg-gradient-to-tr to-dim from-green-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12a1 1 0 0 0-1 1v4a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1v-4a1 1 0 0 0-1-1M5 12h14M5 12a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v4a1 1 0 0 1-1 1m-2 3h.01M14 15h.01M17 9h.01M14 9h.01"/>
                     </svg>
                     <span class="flex-1 ms-3 whitespace-nowrap text-base overflow-auto">Web Hosting Inicia - sale.com </span>
-                    <span class="inline-flex items-center justify-center px-2 py-0.5 sm:p-2 ms-3 text-xs font-medium text-gray-500 bg-gray-200 rounded-sm dark:bg-dark-clear dark:text-secondary">suspended</span>
+                    <span class="w-24 inline-flex items-center justify-center px-2 py-0.5 sm:p-2 ms-3 text-xs font-medium text-heaven bg-yellow-700 rounded-sm">suspended</span>
                 </a>
             </li>
             <li>
-                <a href="#" class="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-dark-brown dark:hover:bg-dark-dim dark:text-white dark:hover:text-secondary">                    
+                <a href="#" class="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-dark-brown/15 dark:hover:bg-dark-brown/50 dark:text-white">                    
                     <svg class="w-6 h-6 p-1 rounded-full text-heaven bg-gradient-to-tr to-orange-500 from-orange-900" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12a1 1 0 0 0-1 1v4a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1v-4a1 1 0 0 0-1-1M5 12h14M5 12a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v4a1 1 0 0 1-1 1m-2 3h.01M14 15h.01M17 9h.01M14 9h.01"/>
                     </svg>
                     <span class="flex-1 ms-3 whitespace-nowrap text-base overflow-auto">VPS Hosting(Full) - vps.focused.cl</span>
-                    <span class="inline-flex items-center justify-center px-2 py-0.5 sm:p-2 ms-3 text-xs font-medium text-gray-500 bg-gray-200 rounded-sm dark:bg-dark-heaven dark:text-secondary">active</span>
+                    <span class="w-24 inline-flex items-center justify-center px-2 py-0.5 sm:p-2 ms-3 text-xs font-medium text-gray-500 bg-gray-200 rounded-sm dark:bg-dark-heaven dark:text-secondary">active</span>
                 </a>
             </li>
             <li>
-                <a href="#" class="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-dark-brown dark:hover:bg-dark-dim dark:text-white dark:hover:text-secondary">
+                <a href="#" class="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-dark-brown/15 dark:hover:bg-dark-brown/50 dark:text-white">
                     <svg class="w-6 h-6 p-1 rounded-full text-heaven bg-gradient-to-tr to-blue-400 from-blue-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12a1 1 0 0 0-1 1v4a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1v-4a1 1 0 0 0-1-1M5 12h14M5 12a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v4a1 1 0 0 1-1 1m-2 3h.01M14 15h.01M17 9h.01M14 9h.01"/>
                     </svg>
                     <span class="flex-1 ms-3 whitespace-nowrap text-base overflow-auto">VM Droplet - vm.comercialx.cl</span>
-                    <span class="inline-flex items-center justify-center px-2 py-0.5 sm:p-2 ms-3 text-xs font-medium text-gray-500 bg-gray-200 rounded-sm dark:bg-dark-heaven dark:text-secondary">active</span>   
+                    <span class="w-24 inline-flex items-center justify-center px-2 py-0.5 sm:p-2 ms-3 text-xs font-medium text-gray-500 bg-gray-200 rounded-sm dark:bg-dark-heaven dark:text-secondary">active</span>   
                 </a>
             </li>
             <li>
-                <a href="#" class="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-dark-brown dark:hover:bg-dark-dim dark:text-white dark:hover:text-secondary">
+                <a href="#" class="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-dark-brown/15 dark:hover:bg-dark-brown/50 dark:text-white">
                     <svg class="w-6 h-6 p-1 rounded-full text-heaven bg-gradient-to-tr to-purple-500 from-purple-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12a1 1 0 0 0-1 1v4a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1v-4a1 1 0 0 0-1-1M5 12h14M5 12a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v4a1 1 0 0 1-1 1m-2 3h.01M14 15h.01M17 9h.01M14 9h.01"/>
                     </svg>
                     <span class="flex-1 ms-3 whitespace-nowrap text-base overflow-auto">Sitio Web(DC) - www.loremipsum.cl</span>
-                    <span class="inline-flex items-center justify-center px-2 py-0.5 sm:p-2 ms-3 text-xs font-medium text-gray-500 bg-gray-200 rounded-sm dark:bg-dark-heaven dark:text-secondary">active</span>
+                    <span class="w-24 inline-flex items-center justify-center px-2 py-0.5 sm:p-2 ms-3 text-xs font-medium text-gray-500 bg-gray-200 rounded-sm dark:bg-dark-heaven dark:text-secondary">active</span>
                 </a>
             </li>
         </ul>
--- resources/views/components/main-client.blade.php
+++ resources/views/components/main-client.blade.php
@@ -1,4 +1,4 @@
-<main class="p-4 md:ml-64 h-auto pt-20">
+<main class="p-4 pt-20 md:ml-64 h-auto">
   <!-- Quick Access -->
   <x-client-user.quick-access />
   <!-- Search bar -->
@@ -7,14 +7,13 @@
   <x-client-user.carousel-banner />
 
   <div class="grid grid-cols-1 xl:grid-cols-2 gap-4 mb-4">
-    <x-client-user.active-services />  
-
-    <div class="border-2 border-dashed rounded-lg border-gray-300 dark:border-dark-brown h-48 md:h-72"></div>
-    <div class="border-2 border-dashed rounded-lg border-gray-300 dark:border-dark-brown h-48 md:h-72"></div>
-    <div class="border-2 border-dashed rounded-lg border-gray-300 dark:border-dark-brown h-48 md:h-72"></div>
+    <x-client-user.active-services />
+    <x-client-user.recent-invoices />
+    <!-- <div class="border-2 border-dashed rounded-lg border-gray-300 dark:border-dark-brown h-48 md:h-72"></div> -->
   </div>
-  <div
-    class="border-2 border-dashed rounded-lg border-gray-300 dark:border-dark-brown h-96 mb-4"></div>
+  <!-- <div class="border-2 border-dashed rounded-lg border-gray-300 dark:border-dark-brown h-96 mb-4"> -->
+    <x-client-user.last-tickets />
+  <!-- </div> -->
   <div class="grid grid-cols-2 gap-4">
     <div class="border-2 border-dashed rounded-lg border-gray-300 dark:border-dark-brown h-48 md:h-72"></div>
     <div class="border-2 border-dashed rounded-lg border-gray-300 dark:border-dark-brown h-48 md:h-72"></div>
