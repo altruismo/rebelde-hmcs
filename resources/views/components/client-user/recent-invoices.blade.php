cec657a09213df7b9392acd99b184feef9f64231
Felipe M <philbone@focused.cl>
2025-07-13 17:16:43 -0400

    Mejora la responsividad del componente Recent Invoices

--- resources/views/components/client-user/recent-invoices.blade.php
+++ resources/views/components/client-user/recent-invoices.blade.php
@@ -7,60 +7,26 @@
             </a>
         </h5>
         <div class="p-4 pt-1 dark:bg-dark-deep">
-            <p class="text-sm font-normal text-gray-500 dark:text-gray-400">Algunas facturas podrían estar vencidas. Paga ahora para evitar interrupciones.</p>
+            <p class="text-sm font-normal text-gray-500 dark:text-gray-400">Click on invoice for details.</p>
             <ul class="my-4 space-y-3">
                 <li>
-                    <a href="#" class="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-dark-brown/15 dark:hover:bg-dark-brown/50 dark:text-white">
-                        <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 17.345a4.76 4.76 0 0 0 2.558 1.618c2.274.589 4.512-.446 4.999-2.31.487-1.866-1.273-3.9-3.546-4.49-2.273-.59-4.034-2.623-3.547-4.488.486-1.865 2.724-2.899 4.998-2.31.982.236 1.87.793 2.538 1.592m-3.879 12.171V21m0-18v2.2" />
-                        </svg>
-                        <span class="flex-1 ms-3 whitespace-nowrap text-base overflow-auto">#18242 13/07/2025 - $2.990</span>
+                    <a href="#" class="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-dark-brown/15 dark:hover:bg-dark-brown/50 dark:text-white">                        
+                        <span class="flex-1 ms-3 whitespace-nowrap text-xs 2xl:text-base overflow-auto scrollbar">#18242 for $2.990, issued 29/06/2025 was paid on 10/07/2025</span>
                         <span class="w-24 inline-flex items-center justify-center px-2 py-0.5 sm:p-2 ms-3 text-xs font-medium text-heaven bg-green-800 rounded-sm">paid</span>
                     </a>
                 </li>
                 <li>
-                    <a href="#" class="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-dark-brown/15 dark:hover:bg-dark-brown/50 dark:text-white">
-                        <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 17.345a4.76 4.76 0 0 0 2.558 1.618c2.274.589 4.512-.446 4.999-2.31.487-1.866-1.273-3.9-3.546-4.49-2.273-.59-4.034-2.623-3.547-4.488.486-1.865 2.724-2.899 4.998-2.31.982.236 1.87.793 2.538 1.592m-3.879 12.171V21m0-18v2.2" />
-                        </svg>
-                        <span class="flex-1 ms-3 whitespace-nowrap text-base overflow-auto">#18151 02/06/2025 - $7.990</span>
+                    <a href="#" class="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-dark-brown/15 dark:hover:bg-dark-brown/50 dark:text-white">                        
+                        <span class="flex-1 ms-3 whitespace-nowrap text-xs 2xl:text-base overflow-auto scrollbar">#18242 for $2.990, issued 01/05/2025 has limit until 14/05/2025</span>
                         <span class="w-24 inline-flex items-center justify-center px-2 py-0.5 sm:p-2 ms-3 text-xs font-medium text-heaven bg-yellow-700 rounded-sm">unpaid</span>
                     </a>
-                </li>
-                <li>
-                    <a href="#" class="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-dark-brown/15 dark:hover:bg-dark-brown/50 dark:text-white">
-                        <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 17.345a4.76 4.76 0 0 0 2.558 1.618c2.274.589 4.512-.446 4.999-2.31.487-1.866-1.273-3.9-3.546-4.49-2.273-.59-4.034-2.623-3.547-4.488.486-1.865 2.724-2.899 4.998-2.31.982.236 1.87.793 2.538 1.592m-3.879 12.171V21m0-18v2.2" />
-                        </svg>
-                        <span class="flex-1 ms-3 whitespace-nowrap text-base overflow-auto">VPS Hosting(Full) - vps.focused.cl</span>
-                        <span class="w-24 inline-flex items-center justify-center px-2 py-0.5 sm:p-2 ms-3 text-xs font-medium text-heaven bg-red-800 rounded-sm">overdue</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#" class="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-dark-brown/15 dark:hover:bg-dark-brown/50 dark:text-white">
-                        <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 17.345a4.76 4.76 0 0 0 2.558 1.618c2.274.589 4.512-.446 4.999-2.31.487-1.866-1.273-3.9-3.546-4.49-2.273-.59-4.034-2.623-3.547-4.488.486-1.865 2.724-2.899 4.998-2.31.982.236 1.87.793 2.538 1.592m-3.879 12.171V21m0-18v2.2" />
-                        </svg>
-                        <span class="flex-1 ms-3 whitespace-nowrap text-base overflow-auto">VM Droplet - vm.comercialx.cl</span>
-                        <span class="w-24 inline-flex items-center justify-center px-2 py-0.5 sm:p-2 ms-3 text-xs font-medium text-gray-500 bg-gray-200 rounded-sm dark:bg-dark-heaven dark:text-secondary">paid</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#" class="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-dark-brown/15 dark:hover:bg-dark-brown/50 dark:text-white">
-                        <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 17.345a4.76 4.76 0 0 0 2.558 1.618c2.274.589 4.512-.446 4.999-2.31.487-1.866-1.273-3.9-3.546-4.49-2.273-.59-4.034-2.623-3.547-4.488.486-1.865 2.724-2.899 4.998-2.31.982.236 1.87.793 2.538 1.592m-3.879 12.171V21m0-18v2.2" />
-                        </svg>
-                        <span class="flex-1 ms-3 whitespace-nowrap text-base overflow-auto">Sitio Web(DC) - www.loremipsum.cl</span>
-                        <span class="w-24 inline-flex items-center justify-center px-2 py-0.5 sm:p-2 ms-3 text-xs font-medium text-gray-500 bg-gray-200 rounded-sm dark:bg-dark-heaven dark:text-secondary">paid</span>
-                    </a>
-                </li>
-            </ul>
+                </li>                
             <div>
                 <a href="#" class="inline-flex items-center text-xs font-normal text-gray-500 hover:underline dark:text-gray-400">
                     <svg class="w-3 h-3 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7.529 7.988a2.502 2.502 0 0 1 5 .191A2.441 2.441 0 0 1 10 10.582V12m-.01 3.008H10M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                     </svg>
-                    View All Services...
+                    View all invoices...
                 </a>
             </div>
         </div>
