a09fd8e9a4d35f7a4b6fa759b099d523a97df95d
Felipe M <philbone@focused.cl>
2025-07-18 17:39:15 -0400

    Aplica bordes redondados al componente Search Bar, elimina el gap

--- resources/views/components/client-user/search-bar.blade.php
+++ resources/views/components/client-user/search-bar.blade.php
@@ -7,7 +7,7 @@
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11.15 5.6h.01m3.337 1.913h.01m-6.979 0h.01M5.541 11h.01M15 15h2.706a1.957 1.957 0 0 0 1.883-1.325A9 9 0 1 0 2.043 11.89 9.1 9.1 0 0 0 7.2 19.1a8.62 8.62 0 0 0 3.769.9A2.013 2.013 0 0 0 13 18v-.857A2.034 2.034 0 0 1 15 15Z" />
                 </svg>
             </div>
-            <input type="text" id="voice-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primo block w-full ps-10 p-2.5 dark:bg-dark-dim dark:border-dark-clear dark:placeholder-dark-shade dark:text-dark-deep dark:focus:ring-dark-clear dark:focus:border-dark-action" placeholder="Search tópicos, tickets de soporte, facturas, etc..." required />
+            <input type="text" id="voice-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-s-lg focus:ring-primo block w-full ps-10 p-2.5 dark:bg-dark-dim dark:border-dark-clear dark:placeholder-dark-shade dark:text-dark-deep dark:focus:ring-dark-clear dark:focus:border-dark-action" placeholder="Search tópicos, tickets de soporte, facturas, etc..." required />
 
             <button type="button" class="absolute inset-y-0 end-0 flex items-center pe-3">
                 <svg class="w-4 h-4 text-dim dark:text-dark-deep hover:text-deep dark:hover:text-dark-deep" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 20">
@@ -15,7 +15,7 @@
                 </svg>
             </button>
         </div>
-        <button type="submit" class="inline-flex items-center py-2.5 px-3 ms-2 text-sm font-medium text-white bg-secondary rounded-lg border border-secondary hover:bg-dim focus:ring-2 focus:outline-none focus:ring-primo dark:bg-dark-clear dark:hover:bg-dark-action dark:focus:ring-blue-800">
+        <button type="submit" class="inline-flex items-center py-2.5 px-3 text-sm font-medium text-white bg-secondary rounded-e-lg border border-s-0 border-secondary hover:bg-dim focus:ring-2 focus:outline-none focus:ring-primo dark:bg-dark-clear dark:hover:bg-dark-action dark:focus:ring-dark-action dark:border-dark-clear">
             <svg class="w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
             </svg>Search
