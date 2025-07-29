48368406b02029f95ea81be0e7a715f1ac3c58e1
Felipe M <philbone@focused.cl>
2025-07-29 15:44:19 -0400

    Crea un build con la nueva estructura de temas

--- vite.config.js
+++ vite.config.js
@@ -18,19 +18,19 @@ const theme = getEnvValue('THEME_CURRENT');
 console.log('Current Theme: '+theme);
 const cssInputs = [
     `resources/themes/${theme}/admin-area/css/app.css`,
-    `resources/themes/${theme}/client-area/css/app.css`,
+    /* `resources/themes/${theme}/client-area/css/app.css`, */
     `resources/themes/${theme}/guest-area/css/app.css`,
 ];
 
 const jsInputs = [
     `resources/themes/${theme}/admin-area/js/app.js`,
-    `resources/themes/${theme}/client-area/js/app.js`,
+    /* `resources/themes/${theme}/client-area/js/app.js`, */
     `resources/themes/${theme}/guest-area/js/app.js`,
 ];
 
 export default defineConfig({
     plugins: [laravel({
-        input: [cssInputs, jsInputs],
+        input: [...cssInputs, ...jsInputs],
         refresh: true,
     })],
     server: {
