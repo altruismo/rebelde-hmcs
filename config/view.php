13dacb07e1aa347afe36121bd19b6a5002ed27d4
Felipe M <philbone@focused.cl>
2025-07-31 14:28:44 -0400

    Agrega el fichero config/view.php

---
+++ config/view.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Theme Currently Active
+    |--------------------------------------------------------------------------
+    |
+    | You may define the default theme to be used across the application.
+    | This value is pulled from the .env file or defaults to 'classic'.
+    |
+    */
+
+     'theme' => 'classic', // aquí defines el nombre del tema activo
+
+];
